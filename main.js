f5b7481358eb035c79864835b8dcefa72268de28
francoheredev <franco_mapa@outlook.com>
2026-05-03 19:12:35 -0300

    resuelto ejercicio2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Guía JavaScript iniciada");
+// --------------------
+// EJERCICIO 2
+// --------------------
+let puntosBase = 100;
+let bonus = 50;
+let penalizacion = 20;
+
+let puntajeFinal = puntosBase + bonus - penalizacion;
+
+console.log("Puntaje final:", puntajeFinal);
