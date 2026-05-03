735735d8aea8e46ffec7c97a51d1bff5cfcd01b9
francoheredev <franco_mapa@outlook.com>
2026-05-03 20:46:38 -0300

    ejercicio-6 listo

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Guía JavaScript iniciada");
+const input = document.getElementById("dato");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+  const opcion = input.value.toLowerCase();
+
+  switch (opcion) {
+    case "iniciar":
+      mensaje.textContent = "Iniciando juego...";
+      break;
+
+    case "configuracion":
+      mensaje.textContent = "Abriendo configuración...";
+      break;
+
+    case "creditos":
+      mensaje.textContent = "Mostrando créditos...";
+      break;
+
+    case "salir":
+      mensaje.textContent = "Saliendo del juego...";
+      break;
+
+    default:
+      mensaje.textContent = "Opción inválida";
+  }
+});
