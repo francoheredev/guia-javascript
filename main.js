1a5b1a4f8a24c910496def64dfe2e22cafe0a3fd
francoheredev <franco_mapa@outlook.com>
2026-05-03 19:28:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Guía JavaScript iniciada");
+const input = document.getElementById("dato");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+  const valor = input.value;
+
+  const numero = Number(valor); // convertir a número
+  const resultado = numero + 10;
+
+  mensaje.textContent = "Resultado: " + resultado;
+});
