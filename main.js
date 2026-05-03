bf31e9a9b5470f880f7b41f725347e993f0aa9e7
francoheredev <franco_mapa@outlook.com>
2026-05-03 19:48:05 -0300

    corrección3

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const mensaje = document.getElementById("mensaje");
 boton.addEventListener("click", function () {
   const valor = input.value;
 
-  const numero = Number(valor); // convertir a número
-  const resultado = numero + 10;
+  const numero = Number(valor);
 
-  mensaje.textContent = "Resultado: " + resultado;
+  if (isNaN(numero)) {
+    mensaje.textContent = "Ingresá un número válido";
+  } else {
+    const resultado = numero + 10;
+    mensaje.textContent = "Resultado: " + resultado;
+  }
 });
