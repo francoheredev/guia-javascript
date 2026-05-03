8555a88724a385431837b2e68897a546b348caa4
francoheredev <franco_mapa@outlook.com>
2026-05-03 20:52:17 -0300

    ejercicio7 resuelto

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Guía JavaScript iniciada");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+  mensaje.textContent = "";
+
+  for (let i = 3; i >= 1; i--) {
+    setTimeout(() => {
+      mensaje.textContent = i;
+    }, (3 - i) * 1000);
+  }
+
+  setTimeout(() => {
+    mensaje.textContent = "¡Comienza!";
+  }, 3000);
+});
