3f7b3eecccf60a231c2cb3e380004f764eaee333
francoheredev <franco_mapa@outlook.com>
2026-05-03 22:43:31 -0300

    ejercicio-26

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Guía JavaScript iniciada");
+const jugador = {
+ nombre: "Luna",
+ puntaje: 1500
+};
+
+
+localStorage.setItem("datosJugador", JSON.stringify(jugador));
+
+
+const jugadorRecuperado = JSON.parse(localStorage.getItem("datosJugador"));
+
+console.log("Objeto recuperado:", jugadorRecuperado);
+console.log("Nombre del jugador:", jugadorRecuperado.nombre);
