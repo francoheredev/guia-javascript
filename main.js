636ac30261e1a91ca46094af636650ac2c4057b9
francoheredev <franco_mapa@outlook.com>
2026-05-03 22:18:14 -0300

    ejercicio-18

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Guía JavaScript iniciada");
+for (let i = 0; i < personajes.length; i++) {
+  let personaje = personajes[i];
+
+  console.log(
+    personaje.nombre +
+    " es " +
+    personaje.tipo +
+    " y está en nivel " +
+    personaje.nivel
+  );
+}
