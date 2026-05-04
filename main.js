9303f2f79e148fb56001042ffbb361a244b7e885
francoheredev <franco_mapa@outlook.com>
2026-05-03 21:48:20 -0300

    ejercicio11

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Guía JavaScript iniciada");
+const mensaje = document.getElementById("mensaje");
+
+
+// agregar elementos
+mochila.push("linterna");
+mochila.push("comida");
+mochila.push("mapa");
+
+// quitar último elemento
+mochila.pop();
+
+// mostrar en pantalla
+mensaje.textContent = "Mochila final: " + mochila.join(", ");
