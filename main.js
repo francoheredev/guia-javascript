8bd16cf6a8d7a1026f17c781281f1784205186cb
francoheredev <franco_mapa@outlook.com>
2026-05-03 21:59:07 -0300

    ejercicio 14

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Guía JavaScript iniciada");
+function calcularVidaRestante(vida, danio) {
+  return vida - danio;
+}
+
+let resultado = calcularVidaRestante(100, 30);
+
+console.log(resultado);
