b1fc2cdea15000d7f2debd3633a9c8964b546cc2
francoheredev <franco_mapa@outlook.com>
2026-05-03 21:52:44 -0300

    ejercicio12

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Guía JavaScript iniciada");
+const mensaje = document.getElementById("mensaje");
+
+if (inventario.includes("llave")) {
+  mensaje.textContent = "La puerta se abre";
+} else {
+  mensaje.textContent = "Necesitás una llave";
+}
