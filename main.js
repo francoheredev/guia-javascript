a76a8eccd058e203e3449ed08ebf1da3c84da3b6
francoheredev <franco_mapa@outlook.com>
2026-05-03 21:01:58 -0300

    ejercicio 8 hecho

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Guía JavaScript iniciada");
+const mensaje = document.getElementById("mensaje");
+
+let puntajeTotal = 0;
+
+for (let ronda = 1; ronda <= 5; ronda++) {
+  puntajeTotal += ronda * 10;
+  
+}
+
+mensaje.textContent = "Puntaje total: " + puntajeTotal;
