343a1ff4ed2c3c653799130aeda7aec1177595eb
francoheredev <franco_mapa@outlook.com>
2026-05-03 22:05:43 -0300

    ejercicio16

--- data.js
+++ data.js
@@ -1,28 +1,7 @@
-const personajes = [
- {
-   id: 1,
-   nombre: "Luna",
-   tipo: "maga",
-   vida: 100,
-   energia: 80,
-   nivel: 3
- },
- {
-   id: 2,
-   nombre: "Kai",
-   tipo: "guerrero",
-   vida: 120,
-   energia: 60,
-   nivel: 4
- },
- {
-   id: 3,
-   nombre: "Nina",
-   tipo: "arquera",
-   vida: 90,
-   energia: 100,
-   nivel: 2
- }
-];
-
-const inventarioBase = ["espada", "poción", "escudo", "llave"];
+const jugador = {
+ nombre: "Luna",
+ vida: 100,
+ energia: 80,
+ nivel: 1,
+ inventario: ["espada", "poción"]
+};
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Guía JavaScript iniciada");
+const mensaje = document.getElementById("mensaje");
+
+mensaje.innerHTML = `
+Nombre: ${jugador.nombre} <br>
+Vida: ${jugador.vida} <br>
+Energía: ${jugador.energia} <br>
+Nivel: ${jugador.nivel} <br>
+Inventario: ${jugador.inventario.join(", ")}
+`;
