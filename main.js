baa9d10f9d0e679a79d79ff4a3d3c1cfeae7a65a
francoheredev <franco_mapa@outlook.com>
2026-05-03 22:37:54 -0300

    ejercicio 25 hecho

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-boton.addEventListener("click", function () {
-  const nombre = input.value.trim();
+const input = document.getElementById("dato");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
 
+boton.addEventListener("click", function () {
+  // Obtenemos el valor actual del input
+  const nombre = input.value;
+
+  // Validamos si el contenido está vacío
   if (nombre === "") {
     mensaje.textContent = "Ingresá un nombre para continuar";
   } else {
+    // Si tiene texto, mostramos el saludo
     mensaje.textContent = "Bienvenido/a, " + nombre;
   }
 });
