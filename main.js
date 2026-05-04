7584453c79be4af7b2490143190a786b057f3cb6
francoheredev <franco_mapa@outlook.com>
2026-05-03 21:06:23 -0300

    ejercicio-9

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Guía JavaScript iniciada");
+const mensaje = document.getElementById("mensaje");
+
+let primero = inventario[0];
+let ultimo = inventario[inventario.length - 1];
+let cantidad = inventario.length;
+
+mensaje.textContent =
+  "Primer objeto: " + primero +
+  " | Último objeto: " + ultimo +
+  " | Cantidad: " + cantidad;
