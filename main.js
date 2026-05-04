ea0a3f8a9bf9a3d82b9d7b612c5d7b6a80539e21
francoheredev <franco_mapa@outlook.com>
2026-05-03 22:30:02 -0300

    ejercicio-24

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Guía JavaScript iniciada");
+const boton = document.getElementById("btn");
+const mensaje = document.getElementById("mensaje");
+
+let puntos = 0;
+
+boton.addEventListener("click", function () {
+  puntos += 10;
+  mensaje.textContent = "Puntaje: " + puntos;
+});
